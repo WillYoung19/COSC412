7523cebe22edbbc3edfbede06fc17697a4cfca05
Brandon82 <83241992+Brandon82@users.noreply.github.com>
2022-05-05 16:27:45 -0400

    Add files via upload

---
+++ TUeats/OrderInfoScreen.swift
@@ -0,0 +1,27 @@
+//
+//  CurrentOrderScreen.swift
+//  TUeats
+//
+//  Created by user219074 on 5/1/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+struct OrderInfoScreen: View {
+    
+    @State private var fname: String = ""
+    @State private var lname: String = ""
+    @State private var TUID: String = ""
+
+    
+    var body: some View {
+        ZStack{
+            Color.darkGray2.edgesIgnoringSafeArea(.all)
+            
+            
+        }
+    }
+}
