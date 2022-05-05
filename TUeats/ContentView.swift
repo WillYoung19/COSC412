f6895c41915711589d71ef6a1641577f1da36a58
cconove1 <104680123+cconove1@users.noreply.github.com>
2022-05-05 12:21:32 -0400

    Add files via upload
    Trying to add ContentView update

--- TUeats/ContentView.swift
+++ TUeats/ContentView.swift
@@ -8,13 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var navigateTo: AnyView?
+    @State private var isActive = false
+    
     var body: some View {
-        LoginScreen()
+        NavigationView {
+            ZStack {
+                Menu {
+                    Button {
+                        self.navigateTo = AnyView(MainScreen())
+                        self.isActive = true
+                    } label: {
+                        Text("Home")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .background(Color.darkGray4)
+                    }
+                    Button {
+                        self.navigateTo = AnyView(CurrentOrderScreen())
+                        self.isActive = true
+                    } label: {
+                        Text("Current Orders")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .background(Color.darkGray4)
+                    }
+                    Button {
+                        self.navigateTo = AnyView(HelpScreen())
+                        self.isActive = true
+                    } label: {
+                        Text("Help")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .background(Color.darkGray4)
+                    }.padding(.trailing, 290).offset(y: -100)
+
+                } label: {
+                    Text("")
+                    Image("icons8-menu-50")
+                }
+                .frame(width: 50, height: 50)
+                .padding(.trailing, 290)
+                .offset(y: -350)
+                .zIndex(1)
+                .background(
+                    NavigationLink(destination: self.navigateTo, isActive: $isActive) {
+                        EmptyView()
+                    })
+                
+                LoginScreen()
+                
+            }.background(Color.darkGray)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        //LoginScreen()
     }
 }
